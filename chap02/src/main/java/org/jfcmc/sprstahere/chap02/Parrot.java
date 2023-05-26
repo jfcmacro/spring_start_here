dfa3931d817b99c4d3a0ad684838c4d916dab909
Juan Cardona (@EPAM) <juan_cardona@epam.com>
2023-05-26 15:06:36 -0500

    Adding Example ch02-ex07

--- chap02/src/main/java/org/jfcmc/sprstahere/chap02/Parrot.java
+++ chap02/src/main/java/org/jfcmc/sprstahere/chap02/Parrot.java
@@ -1,5 +1,6 @@
 package org.jfcmc.sprstahere.chap02;
 
+import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,6 +10,11 @@ public class Parrot {
 
     public String getName() {
         return name;
+    }
+
+    @PostConstruct
+    public void init() {
+        this.name = "Kiki";
     }
 
     public void setName(String name) {
