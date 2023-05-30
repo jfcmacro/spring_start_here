fbe04f6d0c49c9cda6d70638f05f8b2c49c80167
Juan Cardona (@EPAM) <juan_cardona@epam.com>
2023-05-30 15:20:55 -0500

    Adding Example cha04-ex03

--- chap04/src/main/java/org/jfcmc/sprstahere/chap04/services/CommentService.java
+++ chap04/src/main/java/org/jfcmc/sprstahere/chap04/services/CommentService.java
@@ -4,19 +4,16 @@ import org.jfcmc.sprstahere.chap04.model.Comment;
 import org.jfcmc.sprstahere.chap04.proxies.CommentNotificationProxy;
 import org.jfcmc.sprstahere.chap04.repositories.CommentRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Component
 public class CommentService {
 
-    private final CommentRepository commentRepository;
+    @Autowired
+    private CommentRepository commentRepository;
 
-    private final CommentNotificationProxy commentNotificationProxy;
-
-    public CommentService(CommentRepository commentRepository,
-                          CommentNotificationProxy commentNotificationProxy) {
-        this.commentRepository = commentRepository;
-        this.commentNotificationProxy = commentNotificationProxy;
-    }
+    @Autowired
+    private CommentNotificationProxy commentNotificationProxy;
 
     public void publishComment(Comment comment) {
         commentRepository.storeComment(comment);
