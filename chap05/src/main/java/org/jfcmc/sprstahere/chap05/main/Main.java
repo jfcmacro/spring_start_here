9d349eb05bc78cf8fe85422ede62dffc507973e4
Juan Cardona (@EPAM) <juan_cardona@epam.com>
2023-05-31 18:23:25 -0500

    Adding Example cha05-ex04

--- chap05/src/main/java/org/jfcmc/sprstahere/chap05/main/Main.java
+++ chap05/src/main/java/org/jfcmc/sprstahere/chap05/main/Main.java
@@ -2,10 +2,15 @@ package org.jfcmc.sprstahere.chap05.main;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.jfcmc.sprstahere.chap05.config.ProjectConfig;
+import org.jfcmc.sprstahere.chap05.services.CommentService;
 
 public class Main {
 
   public static void main(String[] args) {
     var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+    System.out.println("Before retrieving the CommentService");
+    var service = c.getBean(CommentService.class);
+    System.out.println("After retrieving the CommentService");
   }
 }
