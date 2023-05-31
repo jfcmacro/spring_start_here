222cda5ea33422ea858f33a61b3e60a38115de1e
Juan Cardona (@EPAM) <juan_cardona@epam.com>
2023-05-31 18:38:24 -0500

    Adding Example cha05-ex05

--- chap05/src/main/java/org/jfcmc/sprstahere/chap05/services/CommentService.java
+++ chap05/src/main/java/org/jfcmc/sprstahere/chap05/services/CommentService.java
@@ -1,12 +1,4 @@
 package org.jfcmc.sprstahere.chap05.services;
 
-import org.springframework.stereotype.Service;
-import org.springframework.context.annotation.Lazy;
-
-@Service
-@Lazy
 public class CommentService {
-    public CommentService() {
-        System.out.println("Comment Service instance created!");
-    }
 }
