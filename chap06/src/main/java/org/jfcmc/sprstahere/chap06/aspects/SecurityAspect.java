e358e1ca6ceeccb9ae22debc9c6425b4f6299570
Juan Francisco Cardona McCormick <jfcmacro@gmail.com>
2024-01-02 17:31:08 -0500

    Adding Example cha06-ex07

--- chap06/src/main/java/org/jfcmc/sprstahere/chap06/aspects/SecurityAspect.java
+++ chap06/src/main/java/org/jfcmc/sprstahere/chap06/aspects/SecurityAspect.java
@@ -3,9 +3,11 @@ package org.jfcmc.sprstahere.chap06.aspects;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
 import java.util.logging.Logger;
 
 @Aspect
+@Order(1)
 public class SecurityAspect {
 
     private Logger logger = Logger.getLogger(SecurityAspect.class.getName());
