282102b14f8955e3f661e93164d5fb2866d335c2
Juan Francisco Cardona McCormick <jfcmacro@gmail.com>
2024-02-05 17:12:49 -0500

    Adding Exampleo cha08-ex01

--- chap08/src/main/java/org/jfcmc/sprstahere/chap08/MainController.java
+++ chap08/src/main/java/org/jfcmc/sprstahere/chap08/MainController.java
@@ -3,13 +3,15 @@ package org.jfcmc.sprstahere.chap08;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
     @RequestMapping("/home")
-    public String home(Model model) {
+    public String home(@RequestParam String color,
+		       Model model) {
 	model.addAttribute("username", "Katy");
-	model.addAttribute("color", "red");
+	model.addAttribute("color", color);
 	return "home.html";
     }
 }
