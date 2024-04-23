6e70ad767ec42702a7f64065dd901c6b1704dccb
Juan Francisco Cardona McCormick <jfcmacro@gmail.com>
2024-04-23 18:37:23 -0500

    Adding Example cha08-ex06

--- chap08/src/main/java/org/jfcmc/sprstahere/chap08/ProductsController.java
+++ chap08/src/main/java/org/jfcmc/sprstahere/chap08/ProductsController.java
@@ -2,7 +2,8 @@ package org.jfcmc.sprstahere.chap08;
 
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,7 +15,7 @@ public class ProductsController {
 	this.productService = productService;
     }
 
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public String viewProducts(Model model) {
 	var products = productService.findAll();
 	model.addAttribute("products", products);
@@ -22,15 +23,18 @@ public class ProductsController {
 	return "products.html";
     }
 
-    @RequestMapping(path = "/products",
-		    method = RequestMethod.POST)
-    public String addProduct(@RequestParam String name,
-			     @RequestParam double price,
+    // @RequestMapping(path = "/products",
+    // 		    method = RequestMethod.POST)
+    // public String addProduct(@RequestParam String name,
+    // 			     @RequestParam double price,
+    @PostMapping(path = "/products")
+    public String addProduct(Product p,
 			     Model model) {
-	Product p = new Product();
-	p.setName(name);
-	p.setPrice(price);
+	// Product p = new Product();
+	// p.setName(name);
+	// p.setPrice(price);
 
+	
 	productService.addProduct(p);
 
 	var products = productService.findAll();
