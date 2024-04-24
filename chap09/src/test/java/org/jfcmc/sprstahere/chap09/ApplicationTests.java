55a558f28cd26edd0fdd3e10425b87b130dba76e
Juan Francisco Cardona McCormick <jfcmacro@gmail.com>
2024-04-23 20:53:01 -0500

    Adding Example cha09-ex01

---
+++ chap09/src/test/java/org/jfcmc/sprstahere/chap09/ApplicationTests.java
@@ -0,0 +1,12 @@
+package org.jfcmc.sprstahere.chap09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
